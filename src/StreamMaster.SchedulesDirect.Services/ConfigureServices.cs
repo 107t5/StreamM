4ac5d6f9736fc2c75125e4441fc83a23d25583dd
Carl Reid <kontakt@carlreid.dk>
2025-05-27 16:49:00 +0200

    refactor: move client registration to SD

--- src/StreamMaster.SchedulesDirect.Services/ConfigureServices.cs
+++ src/StreamMaster.SchedulesDirect.Services/ConfigureServices.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Net.Http.Headers;
 
 namespace StreamMaster.SchedulesDirect.Services;
 
@@ -6,6 +8,20 @@ public static class ConfigureServices
 {
     public static IServiceCollection AddSchedulesDirectAPIServices(this IServiceCollection services)
     {
+        services.AddHttpClient(nameof(SchedulesDirectHttpService), client =>
+        {
+            client.BaseAddress = new Uri("https://json.schedulesdirect.org/20141201/");
+            client.Timeout = TimeSpan.FromSeconds(30);
+            client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+            client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
+            client.DefaultRequestHeaders.ExpectContinue = true;
+        })
+        .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+        {
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+            AllowAutoRedirect = true,
+        })
+
         return services
             .AddSingleton<ISchedulesDirectAPIService, SchedulesDirectAPIService>()
             .AddSingleton<ISchedulesDirectRepository, SchedulesDirectRepository>()
